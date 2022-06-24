51473bb67a77e9d3e0f87330f5adc5dc12b31782
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-06-24 11:25:39 +0000

    completed lab

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  // iterate from the beginning of the string to the middle of the string
+  for(let i = 0; i <word.length / 2; i++){
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  const l = word.length - 1 - i;
+  // if the letters don't match, return false
+  if(word[i] !== word[l]){
+    return false;
+}  
+}
+  // if we reach the middle, and all the letters match, return true  
+    return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate through your string, if the first, second and third letter meets the condition
+  return true
+  else
+  return false
+
+  // iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
+  // if we reach the middle, and all the letters match, return true
 */
 
 /*
